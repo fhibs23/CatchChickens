a501378bf6bd0e3f28a192ed4f6a58d550c02bf4
Katya Biryukova <56554278+NASTYAKATYA@users.noreply.github.com>
2022-06-01 23:14:30 +0300

    New sounds + menu scripts

---
+++ Assets/CloseScript2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseScript2 : MonoBehaviour
+{
+    public void CloseScene()
+    {
+        Application.LoadLevel("Scene1");
+    }
+}
